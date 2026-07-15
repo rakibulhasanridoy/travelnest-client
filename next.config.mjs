1f4bb6270b296f223e238f62aa0bc628beac8f76
unknown <rakibulhasanridoy1@gmail.com>
2026-07-15 17:40:38 +0600

    fixing bugs

--- next.config.mjs
+++ next.config.mjs
@@ -14,11 +14,10 @@ const nextConfig = {
     return [
       {
         source:      "/api/:path*",
--       destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"}/api/:path*`,
-+       destination: `${process.env.API_SERVER_URL || "http://localhost:5000"}/api/:path*`,
+        destination: `${process.env.API_SERVER_URL || "http://localhost:5000"}/api/:path*`,
       },
     ];
   },
-};
+};                        
 
-export default nextConfig;
+export default nextConfig; 
